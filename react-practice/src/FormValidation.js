bb85d198a902c99fc8dab6bb5a4e7b5373c707c4
Julia Darwish <darwishjulia4@gmail.com>
2024-04-02 15:31:10 -0400

    Update FormValidation

--- react-practice/src/FormValidation.js
+++ react-practice/src/FormValidation.js
@@ -1,105 +1,45 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from "react";
 
 const FormValidation = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
-  const[errors, setErrors]= useState({});
-  const handleChange = (e) => {
-    const {name, value} = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const validationErrors = {};
-    if (!formData.username.trim()) {
-      validationErrors.username = "Username is required";
-    }
-    if (!formData.email.trim()) {
-      validationErrors.email = "Email is required";
-    }
-    if (!formData.password.trim()) {
-      validationErrors.password = "Password is required";
-    }else if(formData.password.length <6) {
-         validationErrors.password = "Password should be at least 6 characters";
-        }
-
-     if (!formData.confirmPassword.trim()) {
-       validationErrors.confirmPassword = "Confirm password is required";
-     }else if (formData.confirmPassword.length < 6) {
-       validationErrors.confirmPassword = "Password should be at least 6 characters";
-     }
-
-     if(formData.password !== formData.confirmPassword) {
-        validationErrors.password = "Password not matched"
-     }
-     setErrors(validationErrors)
-     if(Object.keys(validationErrors)===0) {
-        alert("Form submitted");
-     }
-  };
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="validation">
-        <h1>My Validation Form</h1>
-        <div className="input-wrapper">
-          <label>Username:</label>
-          <input
-            type="text"
-            name="username"
-            placeholder="username"
-            autoComplete="off"
-            className="input-field"
-            onChange={handleChange}
-          />
-          {errors.username && <span>{errors.username}</span>}
-        </div>
-        <div className="input-wrapper">
-          <label>Email: </label>
-          <input
-            type="email"
-            name="email"
-            placeholder="email@gmail.com"
-            autoComplete="off"
-            className="input-field"
-            onChange={handleChange}
-          />
-          {errors.email && <span>{errors.email}</span>}
-        </div>
-        <div className="input-wrapper">
-          <label>Password: </label>
-          <input
-            type="password"
-            name="password"
-            placeholder="********"
-            autoComplete="off"
-            className="input-field"
-            onChange={handleChange}
-          />
-          {errors.password && <span>{errors.password}</span>}
-        </div>
-        <div className="input-wrapper">
-          <label>Confirm Password: </label>
-          <input
-            type="password"
-            name="confirmPassword"
-            placeholder="********"
-            autoComplete="off"
-            className="input-field"
-            onChange={handleChange}
-          />
-          {errors.confirmPassword && <span>{errors.confirmPassword}</span>}
-        </div>
+    <form>
+      <div>
+        <label>Username</label>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          autoComplete="off"
+        />
       </div>
-      <button type="submit">Submit</button>
+      <div>
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="example@gmail.com"
+          autoComplete="off"
+        />
+      </div>
+      <div>
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          autoComplete="off"
+        />
+      </div>
+      <div>
+        <label>Confirm Password</label>
+        <input
+          type="email"
+          name="confirmPassword"
+          placeholder="Confirm Password"
+          autoComplete="off"
+        />
+      </div>
+      <button>SUBMIT</button>
     </form>
   );
 };
