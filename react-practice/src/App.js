b73825a4b5f15fd14596e7b5899ff52a0e45a431
Julia Darwish <darwishjulia4@gmail.com>
2024-04-07 16:47:27 -0400

    Added Routes

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
-import "./App.css"
 import FormValidation from "./FormValidation";
+import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
 
 
 
 function App() {
   return (
-    <>
-      <header>Registration page</header>
-      <div className="App">
-        <FormValidation />
-      </div>
-    </>
+    // <Link to="/home"></Link>
+    <Router>
+      <Routes>
+    <Route path="/" element={<FormValidation />}/>
+    <Route path="/home-page" element={<HomePage />} />
+  <Route path="/about-page" element={<AboutPage />} /> 
+      </Routes>
+    </Router>
   );
 }
 
